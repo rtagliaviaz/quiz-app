0ba1e3ffb9ddab8b788aebe516f1262a89c15c50
robert <robert5638@gmail.com>
2020-07-07 17:25:26 -0400

    Updates

---
+++ precache-manifest.7af0f266f81d5c74651ee992f742d2ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b00f80185dd212535c0a10592d898ce",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "29b0e60d9cb84acf6c7e",
+    "url": "/quiz-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "95ad52476c8d31f8577c",
+    "url": "/quiz-app/static/css/main.f0a5415d.chunk.css"
+  },
+  {
+    "revision": "29b0e60d9cb84acf6c7e",
+    "url": "/quiz-app/static/js/2.6b91a7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.6b91a7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ad52476c8d31f8577c",
+    "url": "/quiz-app/static/js/main.2638eea0.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
